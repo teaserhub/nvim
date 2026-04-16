891aa382e20d2d6ef4ceb8d939de877793ce9edc
Renat <adman.ufa@gmail.com>
2026-04-17 01:18:34 +0500

    Add: blink grok

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,94 +1,34 @@
+-- lua/plugins/completion.lua
 return {
-  {
-    "saghen/blink.cmp",
-    lazy = false,
-    version = "*",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "rafamadriz/friendly-snippets",
-    },
+  "saghen/blink.cmp",
+  lazy = false,
+  version = "*",
+  dependencies = { "rafamadriz/friendly-snippets" },   -- оставляем
+  config = function()
+    require("blink.cmp").setup({
+      keymap = {
+        preset = "super-tab",      -- самое удобное поведение
+      },
 
-    config = function()
-      local luasnip = require("luasnip")
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
 
-      -- загрузка сниппетов
-      require("luasnip.loaders.from_vscode").lazy_load()
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
 
-      require("blink.cmp").setup({
-
-        -- 🔥 КЛЮЧЕВОЕ: поведение клавиш
-        keymap = {
-          preset = "none",
-
-          ["<Tab>"] = {
-            function(cmp)
-              if cmp.is_visible() then
-                return cmp.select_next()
-              elseif luasnip.expand_or_jumpable() then
-                return luasnip.expand_or_jump()
-              end
-            end,
-            "fallback",
-          },
-
-          ["<S-Tab>"] = {
-            function(cmp)
-              if cmp.is_visible() then
-                return cmp.select_prev()
-              elseif luasnip.jumpable(-1) then
-                return luasnip.jump(-1)
-              end
-            end,
-            "fallback",
-          },
-
-          ["<CR>"] = {
-            function(cmp)
-              if cmp.is_visible() then
-                return cmp.accept()
-              end
-            end,
-            "fallback",
-          },
-
-          ["<C-Space>"] = { "show", "fallback" },
-          ["<C-e>"] = { "hide", "fallback" },
-        },
-
-        appearance = {
-          use_nvim_cmp_as_default = true,
-          nerd_font_variant = "mono",
-        },
-
-        sources = {
-          default = { "lsp", "path", "snippets", "buffer" },
-        },
-
-        snippets = {
-          expand = function(snippet)
-            luasnip.lsp_expand(snippet)
-          end,
-        },
-
-        completion = {
-          menu = {
-            border = "rounded",
-          },
-          documentation = {
-            auto_show = true,
-            auto_show_delay_ms = 200,
-            window = { border = "rounded" },
-          },
-          ghost_text = { enabled = true },
-        },
-
-        signature = {
-          enabled = true,
+      completion = {
+        menu = { border = "rounded" },
+        documentation = {
+          auto_show = true,
           window = { border = "rounded" },
         },
+        ghost_text = { enabled = true },
+      },
 
-        cmdline = { enabled = false },
-      })
-    end,
-  },
+      signature = { enabled = true, window = { border = "rounded" } },
+    })
+  end,
 }
