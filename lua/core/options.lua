e8f145eeeff9c8c29f7224cdfe12fb7ef6e9d5fc
Renat <adman.ufa@gmail.com>
2026-04-17 17:33:46 +0500

    Add: options improve

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -14,6 +14,18 @@ opt.signcolumn = "yes"
 
 opt.clipboard = "unnamedplus"
 opt.scrolloff = 8
+-- ✨ Подсветка скопированного текста (yank)
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+	end,
+})
 
+-- 🔧 Мелкие, но важные улучшения интерфейса
+vim.opt.splitbelow = true -- новые сплиты открываются снизу
+vim.opt.splitright = true -- новые вертикальные сплиты справа
+vim.opt.updatetime = 300 -- быстрее обновление gitsigns, lsp, cursorhold
+vim.opt.completeopt = "menuone,noinsert,noselect" -- улучшение меню cmp
+vim.opt.wildmode = "longest:full,full" -- умное автодополнение в cmdline (:e, :bd и т.д.)
 --opt.showtabline = 2
 --vim.opt.hidden = true -- Оставляет буферы в памяти при переключении
