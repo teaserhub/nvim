ac4ca41da8fd094964cc47e70d05e1ef7bc817ab
Renat <adman.ufa@gmail.com>
2026-04-17 23:10:47 +0500

    Soft delete tabline

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -1,16 +1,18 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
-vim.opt.showtabline = 2
-vim.opt.hidden = true
+-- vim.opt.showtabline = 2
+-- vim.opt.hidden = true
 require("lazy").setup({
-    { import = "plugins" }
+	{ import = "plugins" },
 })
