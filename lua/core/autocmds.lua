b900199a5214ed8bc90d2ae0a1212b3377aff591
Renat <adman.ufa@gmail.com>
2026-04-19 19:11:09 +0500

    Autosave

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,20 +1,59 @@
--- ~/.config/nvim/autocmds.lua
+-- ~/.config/nvim/lua/core/autocmds.lua
 local aucmd = vim.api.nvim_create_autocmd
 
 -- Подсветка скопированного текста
-vim.api.nvim_create_autocmd("TextYankPost", {
+aucmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
     end,
 })
 
 -- Go использует табы, не пробелы
-vim.api.nvim_create_autocmd("FileType", {
+aucmd("FileType", {
     pattern = "go",
     callback = function()
         vim.opt_local.expandtab = false
     end,
 })
+
+-- ✅ АВТОСОХРАНЕНИЕ БУФЕРОВ
+-- При потере фокуса (переключился в другое окно)
+aucmd("FocusLost", {
+    pattern = "*",
+    callback = function()
+        if vim.bo.modified and not vim.bo.readonly and vim.bo.buftype == "" then
+            vim.cmd("silent! wall")
+        end
+    end,
+})
+
+-- При выходе из insert mode (опционально)
+aucmd("InsertLeave", {
+    pattern = "*",
+    callback = function()
+        if vim.bo.modified and not vim.bo.readonly and vim.bo.buftype == "" then
+            vim.cmd("silent! update")
+        end
+    end,
+})
+
+-- Перед выходом из Neovim (на всякий случай)
+aucmd("VimLeavePre", {
+    pattern = "*",
+    callback = function()
+        vim.cmd("silent! wall")
+    end,
+})
+
+-- Автосохранение каждые 3 секунды (если есть изменения)
+local save_timer = vim.loop.new_timer()
+save_timer:start(0, 3000, vim.schedule_wrap(function()
+    local ok = pcall(function()
+        if vim.bo.modified and not vim.bo.readonly and vim.bo.buftype == "" then
+            vim.cmd("silent! update")
+        end
+    end)
+end))
 
 -- -- Вместо автокоманды для Go
 -- vim.api.nvim_create_autocmd("FileType", {
