934756162e8282153749c3896e29ff6ab6189151
Renat <adman.ufa@gmail.com>
2026-04-21 23:52:34 +0500

    Улучшен lualine.lua

--- lua/plugins/specs/lualine.lua
+++ lua/plugins/specs/lualine.lua
@@ -6,19 +6,114 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
             options = {
-                theme = "auto",                    -- авто-тема (подхватит onedark)
-                globalstatus = true,               -- один статуслайн на все окна
-                component_separators = "|",
-                section_separators = "",
+                theme = "auto",                     -- подхватит цвета темы
+                globalstatus = true,                -- единый статусбар
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
+                disabled_filetypes = { "oil", "yazi", "Trouble" },
             },
             sections = {
-                lualine_a = { "mode" },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { "filename" },
-                lualine_x = { "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" },
+                lualine_a = {
+                    {
+                        "mode",
+                        fmt = function(str)
+                            -- Короткие имена режимов
+                            return str:sub(1, 1)
+                        end,
+                        separator = { left = "", right = "" },
+                    },
+                },
+                lualine_b = {
+                    {
+                        "branch",
+                        icon = "",
+                        separator = "",
+                    },
+                    {
+                        "diff",
+                        symbols = {
+                            added    = " ",
+                            modified = " ",
+                            removed  = " ",
+                        },
+                        separator = "",
+                    },
+                },
+                lualine_c = {
+                    {
+                        "diagnostics",
+                        sources = { "nvim_diagnostic" },
+                        symbols = {
+                            error = " ",
+                            warn  = " ",
+                            info  = " ",
+                            hint  = " ",
+                        },
+                        separator = "",
+                    },
+                },
+                lualine_x = {
+                    {
+                        "filetype",
+                        icon_only = false,
+                        separator = "",
+                    },
+                    {
+                        "encoding",
+                        separator = "",
+                        cond = function()
+                            return vim.bo.fenc ~= "utf-8" and vim.bo.fenc ~= ""
+                        end,
+                    },
+                    {
+                        "fileformat",
+                        symbols = {
+                            unix    = "LF",
+                            dos     = "CRLF",
+                            mac     = "CR",
+                        },
+                        separator = "",
+                    },
+                },
+                lualine_y = {
+                    {
+                        "filename",
+                        path = 1,                     -- относительный путь
+                        symbols = {
+                            modified = " ●",
+                            readonly = " ",
+                            unnamed = "[No Name]",
+                        },
+                        separator = "",
+                    },
+                },
+                lualine_z = {
+                    {
+                        "progress",
+                        separator = "",
+                    },
+                    {
+                        "location",
+                        separator = " ",
+                    },
+                    {
+                        function()
+                            return os.date("%H:%M")
+                        end,
+                        separator = { left = "", right = "" },
+                    },
+                },
             },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { "filename" },
+                lualine_x = { "location" },
+                lualine_y = {},
+                lualine_z = {},
+            },
+            tabline = {},
+            extensions = { "fzf", "lazy", "mason", "trouble", "quickfix" },
         },
     },
 }
