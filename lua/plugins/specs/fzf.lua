3e45b6bb29c27c4ed76381f1b23fed72848c994c
Renat <adman.ufa@gmail.com>
2026-04-24 00:54:54 +0500

    ✅ fzf.lua — убрать cmd = FzfLua

--- lua/plugins/specs/fzf.lua
+++ lua/plugins/specs/fzf.lua
@@ -1,7 +1,9 @@
 return {
     "ibhagwan/fzf-lua",
-    cmd = "FzfLua",  -- Явно указываем команду для ленивой загрузки
+    -- cmd = "FzfLua",  ← удалить эту строку
     keys = {
+        { "<C-f>", "<cmd>FzfLua files<CR>", desc = "Find Files" },
+        { "<C-g>", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
         { "<leader>ff", "<cmd>FzfLua files<CR>", desc = "Find Files" },
         { "<leader>fg", "<cmd>FzfLua live_grep<CR>", desc = "Live Grep" },
         { "<leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
